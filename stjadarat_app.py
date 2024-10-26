0d35d1721e9231231a82989f68a24933224d2e85
manal-24 <146840327+manal-24@users.noreply.github.com>
2024-10-27 02:32:56 +0300

    Update stjadarat_app.py

--- stjadarat_app.py
+++ stjadarat_app.py
@@ -5,13 +5,12 @@ from PIL import Image
 image = Image.open('regions2022.png')
 image2 = Image.open('region2023.png')
 
-st.write('a')
-st.markdown("<h1 style='text-align: center;'>المناطق الأكثر توظيفاً في المملكة بين عامي 2022 و2023</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>المناطق الأكثر توظيفاً في المملكة بين عامي 2022-2023</h2>", unsafe_allow_html=True)
 
 st.markdown("<h5 style='text-align: center;'>تسعى المملكة إلى توفير فرص العمل عبر موقع جدارات لتسهيل عملية البحث للباحثين عن العمل. تعتبر منصة جدارات الوطنية الموحدة للتوظيف أداة فعالة تهدف إلى تحسين آلية إعلان الوظائف في جميع القطاعات، مما يسهل على الباحثين عن عمل الوصول إلى الفرص المتاحة</h5>", unsafe_allow_html=True)
 
 # Center the image using Streamlit's layout
-col1, col2, col3 = st.columns([1, 5, 1])  # Create three columns
+col1, col2, col3 = st.columns([1, 6, 1])  # Create three columns
 
 with col1:
     st.write("")  # Empty space in the first column
@@ -24,11 +23,11 @@ with col3:
     st.write("")  # Empty space in the third column
 st.write('ops')
 
-st.markdown("<h5 style='text-align: center;'>تُشكل الرياض 43% من إجمالي الوظائف في المملكة، وذلك بفضل كونها العاصمة وما شهدته من تطوير كبير في مجال الترفيه. فقد ازدادت الفعاليات والمشاريع الترفيهية الجديدة، مما أتاح المزيد من الفرص الوظيفية. تأتي مكة المكرمة في المرتبة الثانية بنسبة 25%، حيث تستقطب المدينة ملايين الزوار سنويًا لأداء مناسك الحج والعمرة، مما يعزز على توفير الوظائف.أما المنطقة الشرقية، فتحتل المرتبة الثالثة بنسبة 16.4% ، إذ تحتوي على أكبر حقول النفط في المملكة وتضم مدينة صناعية مهمة، مما يسهم أيضًا في توفير فرص عمل متعددة. </h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center;'>تُشكل الرياض 40.9% من إجمالي الوظائف في المملكة، وذلك بفضل كونها العاصمة وما شهدته من تطوير كبير في مجال الترفيه. فقد ازدادت الفعاليات والمشاريع الترفيهية الجديدة، مما أتاح المزيد من الفرص الوظيفية. تأتي مكة المكرمة في المرتبة الثانية بنسبة 25.5%، حيث تستقطب المدينة ملايين الزوار سنويًا لأداء مناسك الحج والعمرة، مما يعزز على توفير الوظائف.أما المنطقة الشرقية، فتحتل المرتبة الثالثة بنسبة 14.6% ، إذ تحتوي على أكبر حقول النفط في المملكة وتضم مدينة صناعية مهمة، مما يسهم أيضًا في توفير فرص عمل متعددة. </h5>", unsafe_allow_html=True)
 
 
 # Center the image using Streamlit's layout
-col1, col2, col3 = st.columns([1, 5, 1])  # Create three columns
+col1, col2, col3 = st.columns([1, 6, 1])  # Create three columns
 
 with col1:
     st.write("")  # Empty space in the first column
