ae5aa6198f396c4c4bc07da30b4a18aeac427b59
manal-24 <146840327+manal-24@users.noreply.github.com>
2024-10-27 01:52:09 +0300

    Update stjadarat_app.py

--- stjadarat_app.py
+++ stjadarat_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
+from PIL import Image
+
+# Load the image
+image = Image.open('region2022.jpg')
+image2 = Image.open('region2023.jpg')
+
 st.write('a')
-st.markdown("<h1 style='text-align: center;'>Most Employed regions in the Kingdom between 2022-2023</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>المناطق الأكثر توظيفاً في المملكة بين عامي 2022 و2023</h1>", unsafe_allow_html=True)
+
+# Center the image using Streamlit's layout
+col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns
+
+with col1:
+    st.write("")  # Empty space in the first column
+
+with col2:
+    st.image(image, width=200)  # Center the image in the second column
+    st.image(image2, width=200)
+
+with col3:
+    st.write("")  # Empty space in the third column
+
