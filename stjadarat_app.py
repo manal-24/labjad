8b2712114b6af995aa3b87740380d6e665057065
manal-24 <146840327+manal-24@users.noreply.github.com>
2024-10-27 02:04:17 +0300

    Update stjadarat_app.py

--- stjadarat_app.py
+++ stjadarat_app.py
@@ -8,7 +8,7 @@ image2 = Image.open('region2023.png')
 st.write('a')
 st.markdown("<h1 style='text-align: center;'>المناطق الأكثر توظيفاً في المملكة بين عامي 2022 و2023</h1>", unsafe_allow_html=True)
 
-st.markdown("<h5 style='text-align: center;'>تسعى المملكة إلى توفير فرص العمل عبر موقع جدارات لتسهيل عملية البحث للباحثين عن العمل. تعتبر منصة "جدارات" الوطنية الموحدة للتوظيف أداة فعالة تهدف إلى تحسين آلية إعلان الوظائف في جميع القطاعات، مما يسهل على الباحثين عن عمل الوصول إلى الفرص المتاحة</h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center;'>تسعى المملكة إلى توفير فرص العمل عبر موقع جدارات لتسهيل عملية البحث للباحثين عن العمل. تعتبر منصة **جدارات** الوطنية الموحدة للتوظيف أداة فعالة تهدف إلى تحسين آلية إعلان الوظائف في جميع القطاعات، مما يسهل على الباحثين عن عمل الوصول إلى الفرص المتاحة</h5>", unsafe_allow_html=True)
 
 # Center the image using Streamlit's layout
 col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns
